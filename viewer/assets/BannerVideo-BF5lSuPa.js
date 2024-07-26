1cc4b0de6ff661f33275a49e17613ac5403573a6
Goutham Nadarajan <gouthamr@buffalo.edu>
2024-07-26 16:31:38 +0000

    new timelapse viewer

--- viewer/assets/BannerVideo-BF5lSuPa.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./main-Bv5DBp7O.js";import"./gh_routing-Ccc8RuuD.js";const r="/viewer/assets/banner_video-8h8QZ5tC.mp4";function n(){return e.jsx("video",{autoPlay:!0,loop:!0,muted:!0,children:e.jsx("source",{src:r,type:"video/mp4"})})}export{n as default};
